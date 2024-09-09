18cbcedaf7be0274e61ebc4a414d2a8cbfb626a5
Boy Maas <boy.maas@gmail.com>
2024-09-09 19:33:32 +0200

    feat: add codec and types with deserialization function and tests
    basic framework to test codec deserialization based on type defintion

---
+++ src/codec.zig
@@ -0,0 +1,4 @@
+pub fn deserialize(comptime T: type, data: []u8) !T {
+    _ = data;
+    return error.NotImplemented;
+}
--- src/tests.zig
+++ src/tests.zig
@@ -1,4 +1,5 @@
 comptime {
+    _ = @import("codec_test.zig");
     _ = @import("tests/vectors/codec.zig");
 
     _ = @import("safrole_test.zig");
