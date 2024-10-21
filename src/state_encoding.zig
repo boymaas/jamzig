c06608dd994975a4f9f886685c2207bb12f08d3b
Boy Maas <boy.maas@gmail.com>
2024-10-21 20:44:42 +0200

    refactor: phi encoding

--- src/state_encoding.zig
+++ src/state_encoding.zig
@@ -1,15 +1,18 @@
 const std = @import("std");
 const state = @import("state.zig");
 const alpha = @import("state_encoding/alpha.zig");
+const phi = @import("state_encoding/phi.zig");
 const beta = @import("state_encoding/beta.zig");
 const rho = @import("state_encoding/rho.zig");
 
 pub const encodeAlpha = alpha.encode;
+pub const encodePhi = phi.encode;
 pub const encodeBeta = beta.encode;
 pub const encodeRho = rho.encode;
 
 comptime {
     _ = @import("state_encoding/alpha.zig");
+    _ = @import("state_encoding/phi.zig");
     _ = @import("state_encoding/beta.zig");
     _ = @import("state_encoding/rho.zig");
 }
