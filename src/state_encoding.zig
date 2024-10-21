10f0a470a3a60b385794bf17c73c55c5782d0806
Boy Maas <boy.maas@gmail.com>
2024-10-21 20:39:25 +0200

    refactor: rho encoding

--- src/state_encoding.zig
+++ src/state_encoding.zig
@@ -2,15 +2,14 @@ const std = @import("std");
 const state = @import("state.zig");
 const alpha = @import("state_encoding/alpha.zig");
 const beta = @import("state_encoding/beta.zig");
-const gamma = @import("state_encoding/gamma.zig");
-const delta = @import("state_encoding/delta.zig");
+const rho = @import("state_encoding/rho.zig");
 
 pub const encodeAlpha = alpha.encode;
 pub const encodeBeta = beta.encode;
-pub const encodeGamma = gamma.encode;
-pub const encodeDelta = delta.encode;
+pub const encodeRho = rho.encode;
 
 comptime {
     _ = @import("state_encoding/alpha.zig");
     _ = @import("state_encoding/beta.zig");
+    _ = @import("state_encoding/rho.zig");
 }
