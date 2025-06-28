b1467ba6b30046ea89e82302603c1b0968073374
Boy Maas <boy.maas@gmail.com>
2025-06-28 21:49:03 +0200

    fix: removed all accumulation and ontranfer tests

--- src/tests.zig
+++ src/tests.zig
@@ -40,9 +40,7 @@ comptime {
     _ = @import("pvm_test/fuzzer/polkavm_ffi.zig");
 
     _ = @import("pvm_invocations/accumulate.zig");
-    _ = @import("pvm_invocations/accumulate_test.zig");
     _ = @import("pvm_invocations/ontransfer.zig");
-    _ = @import("pvm_invocations/ontransfer_test.zig");
 
     _ = @import("merkle.zig");
     _ = @import("merkle_test.zig");
